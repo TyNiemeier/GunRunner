e5e9806247ff52842e4f8d3a55cf066dd059d558
KingSomeone <bheald@ftstudent.org>
2025-02-18 14:08:35 -0500

    made it to die

--- enemy.gd
+++ enemy.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 class_name enemy
 
 @export var speed = 50
+
+@export var health = 100
+var damage
 var dead = false
 var player_in_area = false
 var player
@@ -10,10 +13,32 @@ func _ready():
 	dead = false
 
 func _physics_process(delta):
-	if !dead:
+	if dead:
 		$detection_area/CollisionShape2D.disabled = false
 		if player_in_area:
-			position += (player.position - position)
+			position += (player.position - position) / speed
 			$AnimatedSprite.play("Moving")
 		else:
-				$AnimatedSprite.play("Idle")
+			$AnimatedSprite.play("Idle")
+
+	if dead:
+		$detection_area/CollisionShape2D.disabled = true
+
+func _on_dectection_area_body_entered(body):
+	if body.has_method("player"):
+		player_in_area = true
+		player = body
+
+func _on_detection_area_body_exited(body):
+	if body.has_method("player"):
+		player_in_area = false
+
+func take_damage(damage):
+
+	health = health - damage
+	if health <= 0 and !dead:
+		death()
+
+func death():
+	dead = true
+	queue_free()
