ad84da1a97275a3cafe3cab7f243ce4032cbb324
KingSomeone <bheald@ftstudent.org>
2025-02-20 14:07:36 -0500

    enemy chase done

--- TestScene/scripts/enemy_1.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-var speed
-var dead = false
-var player_in_area = false
-var player
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,30 +7,33 @@ class_name enemy
 var damage
 var dead = false
 var player_in_area = false
-var player
+var Player
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	dead = false
 
 func _physics_process(delta):
-	if dead:
-		$detection_area/CollisionShape2D.disabled = false
+	if !dead:
 		if player_in_area:
-			position += (player.position - position) / speed
-			$AnimatedSprite.play("Moving")
+			position += (Player.position - position) / speed
+			sprite.play("Moving")
+			if Player.position < position:
+				sprite.filp_h = true
 		else:
-			$AnimatedSprite.play("Idle")
+			sprite.play("Idle")
 
 	if dead:
 		$detection_area/CollisionShape2D.disabled = true
 
 func _on_dectection_area_body_entered(body):
-	if body.has_method("player"):
+	if body is player:
 		player_in_area = true
-		player = body
+		Player = body
 
 func _on_detection_area_body_exited(body):
-	if body.has_method("player"):
+	if body is player:
 		player_in_area = false
 
 func take_damage(damage):
