e9c852c51f18fbdfec9cf32cbef0991d3567791e
KingSomeone <bheald@ftstudent.org>
2025-04-01 14:09:06 -0400

    enemy 3 shoots fireballs

--- scripts/enemy_3.gd
+++ scripts/enemy_3.gd
@@ -1,11 +1,26 @@
 extends Enemy
 
-func shoot_fireball():
-	pass
+const fireball_scene = preload("res://scenes/entities/Enemy/fireball.tscn")
+@onready var shoot_timer = $ShootTimer
+@onready var rotater = $Rotater
 
-func shoot_cooldown():
-	pass
+const rotate_speed = 0
+const shooter_timer_wait_time = 1
+const spawn_point_count = 8
+const radius = 20
 
+func _ready():
+	var step = 2 * PI / spawn_point_count
+
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		rotater.add_child(spawn_point)
+
+	shoot_timer.wait_time = shooter_timer_wait_time
+	shoot_timer.start()
 
 func _on_fireball_range_body_entered(body):
 	if body is Player:
@@ -15,3 +30,16 @@ func _on_fireball_range_body_entered(body):
 func _on_fireball_range_body_exited(body):
 	if body is Player:
 		in_range = false
+
+func _process(delta: float) -> void:
+	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
+	rotater.rotation_degrees = fmod(new_rotation, 360)
+
+
+func _on_shoot_timer_timeout() -> void:
+	if in_range:
+		for s in rotater.get_children():
+			var fireball = fireball_scene.instantiate()
+			get_tree().root.add_child(fireball)
+			fireball.position = s.global_position
+			fireball.rotation = s.global_rotation # Replace with function body.
