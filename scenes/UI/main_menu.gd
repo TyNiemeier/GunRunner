43762610cbf0e77efced7ac703ca4819f0db6c66
N1tro <robbins_mason@icloud.com>
2025-04-14 14:06:51 -0400

    Added and fixed elements
    Finally added proper buttons. They press the right way and can be disabled which turns them gray. The buttons are coded to go into the main and settings scenes. Quit button works. None of the buttons have words on them. Also added a camera so the buttons are much closer to the screen.

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,11 +1,15 @@
 extends Control
+var button_pressed = false
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/")
+	button_pressed = true
+	get_tree().change_scene_to_file("res://scenes/levels/main.tscn")
 	
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/") 
+	button_pressed = true
+	get_tree().change_scene_to_file("res://Scenes/UI/settings.tscn") 
 
 func _on_quit_button_pressed():
+	button_pressed = true
 	get_tree().quit() 
