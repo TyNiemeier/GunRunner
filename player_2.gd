890f65f48115bfee814dbcf4c5b5d855e32fb92e
TyNiemeier <sctythefly@gmail.com>
2025-04-16 14:06:57 -0400

    imported animations for the second player
    added animations to second player animated sprite

---
+++ player_2.gd
@@ -0,0 +1,212 @@
+extends Player
+
+func _set_direction():
+	if isAttacking == false:
+		if isDashing == false:
+			if direction.x < 0:
+				facing = Directions.LEFT
+				isWalking = true
+			elif direction.x > 0:
+				facing = Directions.RIGHT
+				isWalking = true
+			elif direction.y > 0:
+				facing = Directions.DOWN
+				isWalking = true
+			elif direction.y < 0:
+				facing = Directions.UP
+				isWalking = true
+			if direction.x == 0 && direction.y == 0:
+				isIdle = true
+				isWalking = false
+				isSprinting = false
+			else:
+				isIdle = false
+		else:
+			isSprinting = false
+			isWalking = false
+			isIdle = false
+	else:
+		isWalking = false
+		isIdle = false
+
+
+
+func _direction_suffix():
+	if facing == Directions.LEFT:
+		return "DownLeft"
+	elif facing == Directions.RIGHT:
+		return "DownRight"
+	elif facing == Directions.DOWN:
+		return "Down"
+	elif facing == Directions.UP:
+		return "Up"	
+
+#sets animations for sprinting idle walk dash, etc
+func _set_animation():
+
+	#changes sprinting 
+	if isSprinting && isAttacking == false:
+		#sprinting for spear
+		if currentWeapon == 0:
+			sprite.play("p2_spearRun" + _direction_suffix())
+		#sprinting for gun
+		else:
+			sprite.play("p2_gunRun" + _direction_suffix())
+
+
+	#changes idle animations
+	elif isIdle:
+		#idle for spear
+
+		if currentWeapon == 0:
+			sprite.play("p2_idleSpear" + _direction_suffix())
+		#idle for gun
+		else:
+
+			sprite.play("p2_idleGun" + _direction_suffix())
+	
+
+
+	elif isWalking:
+		#walking for spear
+
+		if currentWeapon == 0:
+			sprite.play("p2_spearWalk" + _direction_suffix())
+		#walking for gun
+		else:
+			sprite.play("p2_gunWalk" + _direction_suffix())
+
+	elif isDashing:
+			#dashing for spear
+
+			if currentWeapon == 0:
+				sprite.play("p2_spearDash" + _direction_suffix())
+			#dashing for gun
+			else:
+				sprite.play("p2_gunDash" + _direction_suffix())
+	
+	if isAttacking:
+			#attack for spear
+		if currentWeapon == 0:
+			sprite.play("p2_spearAttack" + _direction_suffix())
+			#attack for gun
+		else:
+			if isSprinting:
+				sprite.play("p2_gunAttackRun" + _direction_suffix())
+			else:
+				sprite.play("p2_gunAttack" + _direction_suffix())
+			
+
+#ISWALKING IS OVERIDING THE SPRINT POSSIBLY REWRITE SETTING WALKING TO TRUE
+func dash():
+	if (Input.is_action_just_pressed("dash") and canDash):
+		isDashing = true
+		canDash = false
+		dash_duration_timer.start()
+		dash_cool_down_timer.start()
+
+	if isDashing:
+		collision = false
+func _on_dash_duration_timeout():
+	isDashing = false
+	collision = true
+func _on_dash_cool_down_timeout():
+	canDash = true
+
+func _on_animated_sprite_2d_animation_finished():
+	print(sprite.animation)
+	if sprite.animation == "p2_spearAttack" or "p2_gunAttack "or "p2_gunAttackRun" + _direction_suffix():
+		isAttacking = false	
+
+func _physics_process(_delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+	if Input.is_action_just_pressed("p2_x"):
+		currentWeapon += 1
+	if currentWeapon >= 2:
+		currentWeapon = 0
+	direction = Input.get_vector("p2_left", "p2_right", "p2_up", "p2_down")
+	#Movement
+	
+	velocity = direction * SPEED	
+
+	#Sprint
+	if Input.is_action_pressed("p2_a"):
+		SPEED = 200
+		isSprinting = true
+		#print(action)
+	else:
+		SPEED = 150
+		isSprinting = false
+	#Dash
+	if isDashing:
+		velocity = direction * dashSpeed
+	else:
+		velocity = direction * SPEED
+
+	#Attack
+	if Input.is_action_just_pressed("p2_l1"):
+		isAttacking = true
+
+
+
+
+	#health doesnt go above health
+	if health > 100:
+		health = 100
+		print(health)
+
+	_set_direction()
+	_set_animation()
+	move_and_slide()
+	dash()
+
+
+#enemy or projectile hits player
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		enemy_inattack_range = true
+		take_damage = body.damage
+		$PlayerHitbox/Hitboxtimer.start()
+	if body is Projectile:
+		if isDashing == false:
+			enemy_inattack_range = true
+			health -= body.damage
+			print(health)
+		body.queue_free()
+
+#enemy leaves player's hitbox
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body is Enemy:
+		enemy_inattack_range = false
+		$PlayerHitbox/Hitboxtimer.stop()
+
+#player takes damage
+func player_hit(take_damage):
+	if enemy_inattack_range and allow_damage:
+		health -= take_damage
+		allow_damage = false
+		$allow_damage.start()
+		print(health)
+
+#invisable / how long it takes until player is allowed to get damage
+func _on_allow_damage_timeout() -> void:
+	allow_damage = true # Replace with function body.
+
+#how fast they attack
+func _on_hitboxtimer_timeout() -> void:
+	if isDashing == false:
+		player_hit(take_damage)
+
+#player picks up health item
+func _on_player_hitbox_area_entered(area: Area2D) -> void:
+	if area is Healing:
+		if health < 100:
+			health += area.heal
+			area.queue_free()
+			print(health)
+
