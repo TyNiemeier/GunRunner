91bcdd62a61b649de37520b052471d62dcf4b3d9
TyNiemeier <sctythefly@gmail.com>
2025-05-05 14:04:00 -0400

    updated player two code
    updated the second player code but still need to add animations

--- player_2.gd
+++ player_2.gd
@@ -1,33 +1,57 @@
 extends Player
 
 func _set_direction():
-	if isAttacking == false:
-		if isDashing == false:
-			if direction.x < 0:
-				facing = Directions.LEFT
-				isWalking = true
-			elif direction.x > 0:
-				facing = Directions.RIGHT
-				isWalking = true
-			elif direction.y > 0:
-				facing = Directions.DOWN
-				isWalking = true
-			elif direction.y < 0:
-				facing = Directions.UP
-				isWalking = true
-			if direction.x == 0 && direction.y == 0:
-				isIdle = true
-				isWalking = false
-				isSprinting = false
+	if isDead == false:
+		if reloading == false:
+			if isAttacking == false:
+				if isDashing == false:
+					if direction.x < 0 and direction.y >= 0:
+						facing = Directions.LEFT
+						isWalking = true
+						isIdle = false
+					elif direction.x > 0 and direction.y >= 0:
+						facing = Directions.RIGHT
+						isWalking = true
+						isIdle = false
+					elif direction.y < 0 and direction.x == 0:
+						facing = Directions.UP
+						isWalking = true
+						isIdle = false
+					elif direction.y < 0 and direction.x < 0:
+						facing = Directions.UPLEFT
+						isWalking = true
+						isIdle = false
+					elif direction.y < 0 and direction.x > 0:
+						facing = Directions.UPRIGHT
+						isWalking = true
+						isIdle = false
+					elif direction.y > 0 and direction.x == 0:
+						facing = Directions.DOWN
+						isWalking = true
+						isIdle = false
+					if direction.x == 0 && direction.y == 0:
+						isIdle = true
+						isWalking = false
+						isSprinting = false
+				else:
+					isSprinting = false
+					isWalking = false
+					isIdle = false
 			else:
+				isWalking = false
 				isIdle = false
 		else:
-			isSprinting = false
-			isWalking = false
+			isAttacking = false
 			isIdle = false
+			isWalking = false
+			isDashing = false
+			isSprinting = false
 	else:
-		isWalking = false
+		isAttacking = false
 		isIdle = false
+		isWalking = false
+		isDashing = false
+		isSprinting = false
 
 
 
@@ -40,6 +64,10 @@ func _direction_suffix():
 		return "Down"
 	elif facing == Directions.UP:
 		return "Up"	
+	elif facing == Directions.UPLEFT:
+		return "UpLeft"	
+	elif facing == Directions.UPRIGHT:
+		return "UpRight"	
 
 #sets animations for sprinting idle walk dash, etc
 func _set_animation():
@@ -95,6 +123,15 @@ func _set_animation():
 				sprite.play("p2_gunAttackRun" + _direction_suffix())
 			else:
 				sprite.play("p2_gunAttack" + _direction_suffix())
+
+	if isDead:
+		sprite.play("p2_death" + _direction_suffix())
+
+
+	if reloading:
+			#attack for spear
+		if currentWeapon == 1:
+			sprite.play("p2_reloading" + _direction_suffix())
 			
 
 #ISWALKING IS OVERIDING THE SPRINT POSSIBLY REWRITE SETTING WALKING TO TRUE
@@ -117,7 +154,8 @@ func _on_animated_sprite_2d_animation_finished():
 	print(sprite.animation)
 	if sprite.animation == "p2_spearAttack" or "p2_gunAttack "or "p2_gunAttackRun" + _direction_suffix():
 		isAttacking = false	
-
+	if sprite.animation == "p2_death" + _direction_suffix():
+			get_tree().change_scene_to_file("res://scenes/levels/death.tscn")
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -132,7 +170,10 @@ func _physics_process(_delta):
 	direction = Input.get_vector("p2_left", "p2_right", "p2_up", "p2_down")
 	#Movement
 	
-	velocity = direction * SPEED	
+	if canMove:
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO		
 
 	#Sprint
 	if Input.is_action_pressed("p2_a"):
@@ -145,20 +186,37 @@ func _physics_process(_delta):
 	#Dash
 	if isDashing:
 		velocity = direction * dashSpeed
-	else:
-		velocity = direction * SPEED
+
 
 	#Attack
 	if Input.is_action_just_pressed("p2_l1"):
 		isAttacking = true
-
+		if currentWeapon == 0:
+			spear_attack()
+		if currentWeapon == 1:
+			if current_ammo > 0:
+				shoot()
+			if current_ammo == 0:
+				reload()
+	$Aim.look_at((get_global_mouse_position()))
 
 
 
 	#health doesnt go above health
 	if health > 100:
 		health = 100
-		print(health)
+	
+	if Input.is_action_just_pressed("p1_bomb"):
+		if drop_bomb == true:
+			var bomb = bomb_scene.instantiate()
+			get_tree().root.add_child(bomb)
+			bomb.global_position = self.global_position
+			drop_bomb = false
+			$BombCoolDown.start()
+
+	if health <= 0:
+		isDead = true
+		canMove = false
 
 	_set_direction()
 	_set_animation()
@@ -191,7 +249,6 @@ func player_hit(take_damage):
 		health -= take_damage
 		allow_damage = false
 		$allow_damage.start()
-		print(health)
 
 #invisable / how long it takes until player is allowed to get damage
 func _on_allow_damage_timeout() -> void:
@@ -210,3 +267,53 @@ func _on_player_hitbox_area_entered(area: Area2D) -> void:
 			area.queue_free()
 			print(health)
 
+func spear_attack():
+	if isAttacking and currentWeapon == 0:
+		if Directions.RIGHT:
+			var bodies = $Rightattack.get_overlapping_bodies()
+			for body in bodies:
+				if body is Enemy:
+					body.health -= spear_damage
+					print('Rightattack')
+		if Directions.LEFT:
+			var bodies = $Leftattack.get_overlapping_bodies()
+			for body in bodies:
+				if body is Enemy:
+					body.health -= spear_damage
+					print('Leftattack')
+		if Directions.UP:
+			var bodies = $Upattack.get_overlapping_bodies()
+			for body in bodies:
+				if body is Enemy:
+					body.health -= spear_damage
+					print('Upattack')
+		if Directions.DOWN:
+			var bodies = $Downattack.get_overlapping_bodies()
+			for body in bodies:
+				if body is Enemy:
+					body.health -= spear_damage
+					print('Downattack')
+
+func shoot():
+	if isAttacking and currentWeapon == 1:
+		var bullet = bulletPath.instantiate()
+		get_parent().add_child(bullet)
+		bullet.position = $Aim/Marker2D.global_position
+		bullet.velocity = get_global_mouse_position() - bullet.position
+		bullet.rotation = $Aim.rotation
+		current_ammo -= 1
+
+func reload():
+	#if Input.is_action_just_pressed("p1_b"):
+	if reloading == false:
+		reloading = true
+		$Reload.start()
+		
+
+func _on_bomb_cool_down_timeout() -> void:
+	drop_bomb = true
+	$BombCoolDown.stop()
+
+func _on_reload_timeout() -> void:
+	reloading = false
+	current_ammo = 7
